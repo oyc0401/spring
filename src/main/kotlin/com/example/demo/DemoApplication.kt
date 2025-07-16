9e1df7b33ec630cb629c814596708eff3a9c075f
yuchan <oyc0401@gmail.com>
2025-07-16 20:07:50 +0900

    cors

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -1,8 +1,15 @@
 package com.example.demo
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.servers.Server
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+@OpenAPIDefinition(
+    info = Info(title = "API 문서", version = "v1"),
+    servers = [Server(url = "https://test.rokafmail.kr", description = "도메인 설명")]
+)
 @SpringBootApplication
 class DemoApplication
 
