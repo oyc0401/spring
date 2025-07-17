04342638e3ac18d9368d7065386a236b43f6a1a3
yuchan <oyc0401@gmail.com>
2025-07-17 17:18:18 +0900

    createAt 자동 설정

--- src/main/kotlin/com/example/demo/user/User.kt
+++ src/main/kotlin/com/example/demo/user/User.kt
@@ -2,9 +2,11 @@ package com.example.demo.user
 
 import jakarta.persistence.*
 import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
 
 @Entity
+@EntityListeners(AuditingEntityListener::class)
 @Table(name = "users")
 data class User(
     @Id
